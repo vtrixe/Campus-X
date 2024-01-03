ec58c65ba779d23267a35cecd75a110adcda7fb2
VIKRANT SHARMA <vikrant500sharma@gmail.com>
2024-01-04 02:17:16 +0530

    auth setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+const AuthLayout = ({ 
+    children
+  }: { 
+    children: React.ReactNode
+  }) => {
+    return ( 
+      <div >
+        {children}
+      </div>
+     );
+  }
+   
+  export default AuthLayout;
